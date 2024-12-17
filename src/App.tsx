27c3c8b5d8e2069d551228cd4f0b84f96d76bc2d
huangzhuoran <huangzhuoran@haomo-studio.com>
2024-12-17 11:01:00 +0800

    fix: 添加截图

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect }  from "react";
 import "./App.css";
 import { createTheme, ThemeProvider } from "@rneui/themed";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -7,6 +7,16 @@ import HomeScreen from './screens/HomeScreen.tsx';
 const theme = createTheme({});
 
 function App() {
+  useEffect(() => {
+    // 设置定时器，延迟10秒后发送postMessage
+    const timer = setTimeout(() => {
+      window.parent?.postMessage({ 'action': 'on-code-ok' }, '*');
+    }, 10000); // 10000毫秒 = 10秒
+
+    // 清理定时器
+    return () => clearTimeout(timer);
+  }, []); // 空依赖数组意味着只在组件挂载时执行
+  
   return (
     <>
       <style type="text/css">{`
